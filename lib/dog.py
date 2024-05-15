9fc38d1848204d9bdb11daa6d700829099d3fd23
Dan <jacobydan01@gmail.com>
2024-05-15 15:39:00 -0400

    property

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -12,4 +12,33 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    #sets default values for name and breed if none filled in
+    def __init__(self, name = 'Fido', breed = 'Mastiff'):
+        self.name = name
+        self.breed = breed
+    
+    #getter and setter function for name
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if type(name) == str and (1 <= len(name) <= 25):
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+    
+    #checks if dog name is in list of approved breeds 
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    
+    breed = property(get_breed, set_breed)
+
+        
